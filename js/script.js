1505174fad10e4f023239e3ce61f7a876fd0ca0d
Inggar Wangi Rahardini <inggarwangirahardini@gmail.com>
2024-12-06 03:48:23 +0700

    update js fix

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ function validateForm() {
 
     console.log('BB:' + inputBeratBadan);
     console.log('Usia:' + inputUsia);
-    console.log('Tinggi:' + inputTinggiBadan) / 100;
+    console.log('Tinggi:' + inputTinggiBadan);
     if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '' ) {
-        let calculate = inputBeratBadan / (inputTinggiBadan * inputTinggiBadan);
+        let calculate = (inputBeratBadan / (inputTinggiBadan * inputTinggiBadan)).toFixed(1);
         updateResult(calculate);
     } else {
         alert('Data Belum Di Input! Mohon Cek Kembali');
@@ -18,4 +18,14 @@ function validateForm() {
 
 function updateResult(value) {
     document.getElementById('result-calculation').innerHTML = value;
+
+    if (value < 18.5) {
+        document.getElementById('info-result').innerText = "Anda Kekurangan Berat Badan";
+    } else if(value >= 18.5 && value <= 25.0){
+        document.getElementById('info-result').innerText = "Anda Memiliki Berat Badan Ideal";
+    } else if(value >= 25.0 && value <= 30.0){
+        document.getElementById('info-result').innerText = "Anda Kelebihan Berat Badan";
+    } else {
+        document.getElementById('info-result').innerText = "Obesitas";
+    }
 }
