559941242ff289cbe7d905eee2f83f8d1aed010f
Inggar Wangi Rahardini <inggarwangirahardini@gmail.com>
2024-12-06 00:33:26 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // ini fiLe javascript
 
 function validateForm() {
-    let inputBeratBadan = document.getElementById('berat-badan-input').value;
-    let inputUsia = document.getElementById('usia-input').value;
-    let inputTinggiBadan = document.getElementById('tinggi-badan-input').value;
+    let inputBeratBadan = parseInt(document.getElementById('berat-badan-input').value);
+    let inputUsia = parseInt(document.getElementById('usia-input').value);
+    let inputTinggiBadan = parseInt(document.getElementById('tinggi-badan-input').value) / 100;
 
     console.log('BB:' + inputBeratBadan);
     console.log('Usia:' + inputUsia);
-    console.log('Tinggi:' + inputTinggiBadan);
+    console.log('Tinggi:' + inputTinggiBadan) / 100;
     if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '' ) {
-        let calculate = parseInt(inputBeratBadan) + parseInt(inputTinggiBadan);
+        let calculate = inputBeratBadan / (inputTinggiBadan * inputTinggiBadan);
         updateResult(calculate);
     } else {
         alert('Data Belum Di Input! Mohon Cek Kembali');
