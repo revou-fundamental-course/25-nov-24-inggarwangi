e30b70863ed00cdd55071b77b3fc2676032da8e5
Inggar Wangi Rahardini <inggarwangirahardini@gmail.com>
2024-12-05 22:03:21 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // ini fiLe javascript
+
+function validateForm() {
+    let inputBeratBadan = document.getElementById('berat-badan-input').value;
+    let inputUsia = document.getElementById('usia-input').value;
+    let inputTinggiBadan = document.getElementById('tinggi-badan-input').value;
+
+    console.log('BB:' + inputBeratBadan);
+    console.log('Usia:' + inputUsia);
+    console.log('Tinggi:' + inputTinggiBadan);
+    if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '' ) {
+        let calculate = parseInt(inputBeratBadan) + parseInt(inputTinggiBadan);
+        updateResult(calculate);
+    } else {
+        alert('Data Belum Di Input! Mohon Cek Kembali');
+    }
+}
+
+function updateResult(value) {
+    document.getElementById('result-calculation').innerHTML = value;
+}
